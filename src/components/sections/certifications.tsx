cc4d59b40ed5639b6daeba650c20708dc6848c2f
isuryaprakashh <isurya2005@gmail.com>
2025-05-29 10:32:12 +0530

    added salesforce certifictae

--- src/components/sections/certifications.tsx
+++ src/components/sections/certifications.tsx
@@ -20,6 +20,13 @@ const certificationsData = [
     issued: "Apr 2024",
     verificationUrl: "https://archive.nptel.ac.in/content/noc/NOC24/SEM1/Ecertificates/106/noc24-cs43/Course/NPTEL24CS43S26990138930513096.pdf",
     logo: "https://imgs.search.brave.com/_tL_dSXzmOYOKxJpqDhWHPi80xcUfKU-R6LLSZeMNCM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vUU8zdVNs/dmFJcWhDbWRTcVFS/ZWNjanBaT3VWX2dt/TGIzR0w5aUZDaGk0/TS9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNEb3ZMM05o/Ym1kaC9iWFZ1YVha/bGNuTnBkSGt1L1lX/TXVhVzR2ZDNBdFky/OXUvZEdWdWRDOTFj/R3h2WVdSei9Mekl3/TWpNdk1ETXZibkIw/L1pXd3VhbkJu"
+  },
+  {
+    title: "Salesforce Certified AI Associate",
+    issuer: "Salesforce",
+    issued: "Oct 2024",
+    verificationUrl: "https://www.salesforce.com/trailblazer/isuryaprakashh",
+    logo: "https://drm.file.force.com/servlet/servlet.ImageServer?id=0153k00000BEqgH&oid=00DF0000000gZsu&lastMod=1693557495000"
   }
 ]
 
